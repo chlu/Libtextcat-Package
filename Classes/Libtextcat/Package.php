e5a81646e114d2813ced783916582cbde1f1588a
Kay Strobach <info@kay-strobach.de>
2014-05-24 17:02:24 +0200

    CodeMigrations TYPO3 Flow - Readd Classes

---
+++ Classes/Libtextcat/Package.php
@@ -0,0 +1,22 @@
+<?php
+namespace Libtextcat;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Libtextcat".                 *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use \TYPO3\Flow\Package\Package as BasePackage;
+
+/**
+ * The Libtextcat Package
+ */
+class Package extends BasePackage {
+
+}
+?>
